04a169e5df0f11f63bcb7edf91b07bc876f8a084
Tomasz Bazelczuk <tbazelczuk@gmail.com>
2022-09-13 11:19:20 +0200

    add mongoose

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const mailer = require("./server/mailer");
 
+const port = process.env.PORT || 3000
 const app = express()
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('build', options))
+app.use(express.static('build'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.get("/api/sendMail", function (req, res) {
   mailer.sendMail([{ url: "foo", value: 1, prevItem: { value: 2 } }]);
   res.send("sendMail");
 });
-
-const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`React app listening at http://localhost:${port}`)
